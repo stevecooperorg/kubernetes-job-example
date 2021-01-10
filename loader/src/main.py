0b3640555f11940ac4ac07ce0b48d6abcc9f7f10
Steve Cooper <steve.cooper@anaplan.com>
2021-01-10 12:53:59 +0000

    refactoring connection complexity

--- loader/src/main.py
+++ loader/src/main.py
@@ -3,20 +3,25 @@ import pika
 
 # https://www.rabbitmq.com/tutorials/tutorial-one-python.html
 
-work_queue_host = os.environ['WORK_QUEUE_HOST']
-work_queue_port = int(os.environ.get('WORK_QUEUE_PORT', "5672"))
-print("connecting to work queue ", work_queue_host, str(work_queue_port))
-credentials = pika.PlainCredentials('guest', 'guest')
-parameters = pika.ConnectionParameters(work_queue_host, work_queue_port, '/', credentials)
-connection = pika.BlockingConnection(parameters)
+def get_work_queue_channel(queue_name):
+    work_queue_host = os.environ['WORK_QUEUE_HOST']
+    work_queue_port = int(os.environ.get('WORK_QUEUE_PORT', "5672"))
+    print("connecting to work queue ", work_queue_host, str(work_queue_port))
+    credentials = pika.PlainCredentials('guest', 'guest')
+    parameters = pika.ConnectionParameters(work_queue_host, work_queue_port, '/', credentials)
+    connection = pika.BlockingConnection(parameters)
+    print("connected to work queue")
+    channel = connection.channel()
+    channel.queue_declare(queue=queue_name)
+    return channel
 
-print("connected to work queue")
-channel = connection.channel()
-channel.queue_declare(queue='ids')
 
-for x in range(100):
-    print("loading data:", str(x))
-    channel.basic_publish(exchange='',routing_key='ids',body=str(x))
+def main():
+    channel = get_work_queue_channel("ids")
+    for x in range(100):
+        print("loading data:", str(x))
+        channel.basic_publish(exchange='',routing_key='ids',body=str(x))
+    print("loaded data")
 
-connection.close()
-print("loaded data")
+
+main()
--- processor/src/main.py
+++ processor/src/main.py
@@ -4,13 +4,9 @@ import time
 
 def do_work(id):
     print(" [x] Received %r" % id)
+    time.sleep(0.25)
 
-def main():
-
-    user = os.environ['JOB_USER']
-    pwd = os.environ['JOB_PASS']
-    print("credentials:", user, pwd)
-
+def get_work_queue_channel(queue_name):
     work_queue_host = os.environ['WORK_QUEUE_HOST']
     work_queue_port = int(os.environ.get('WORK_QUEUE_PORT', "5672"))
     print("connecting to work queue ", work_queue_host, str(work_queue_port))
@@ -19,9 +15,20 @@ def main():
     connection = pika.BlockingConnection(parameters)
     print("connected to work queue")
     channel = connection.channel()
-    channel.queue_declare(queue='ids')
+    channel.queue_declare(queue=queue_name)
+    return channel
 
-    method_frame, header_frame, id = channel.basic_get('ids')
+
+def main():
+
+    user = os.environ['JOB_USER']
+    pwd = os.environ['JOB_PASS']
+    print("credentials:", user, pwd)
+
+    channel=get_work_queue_channel("ids")
+
+    # loop over every message until it's exhausted the work
+    method_frame, header_frame, id = channel.basic_get("ids")
     while method_frame:
         print("got id", id)
         channel.basic_ack(method_frame.delivery_tag)
